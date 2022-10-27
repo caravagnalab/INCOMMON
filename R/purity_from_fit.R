7e30890957df5debb258ed53640778466a0e7230
nicola-calonaci <nicola.calonaci@gmail.com>
2022-10-27 12:39:31 +0200

    purity estimate fail

--- R/purity_from_fit.R
+++ R/purity_from_fit.R
@@ -1,9 +1,12 @@
-purity_from_fit = function(n_binomials, peaks, purity, eps){
-    if(n_binomials == 3) out = peaks[2] * 2
-    if(n_binomials == 1) out = peaks[1] * 2
-    if(n_binomials == 2 & !is.na(purity) & min(abs(peaks[2] * 2 - purity), abs(peaks[1] * 2 - purity)) <= eps) 
-      out = peaks[which.min(c(abs(peaks[2]*2 - purity), abs(peaks[1] * 2 - purity)))] * 2
-  if(is.na(out)){
+purity_from_fit = function(n_binomials, peaks, purity, eps) {
+  out = NA
+  if (n_binomials == 3)
+    out = peaks[2] * 2
+  if (n_binomials == 1)
+    out = peaks[1] * 2
+  if (n_binomials == 2 & !is.na(purity) & min(abs(peaks[2] * 2 - purity), abs(peaks[1] * 2 - purity)) <= eps)
+    out = peaks[which.min(c(abs(peaks[2] * 2 - purity), abs(peaks[1] * 2 - purity)))] * 2
+  if (is.na(out)) {
     cli::cli_alert("Purity could not be estimated reliably. Check results of the fit in the output figure")
   }
   return(out)
