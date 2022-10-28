dbae768bdceaef3aa5a8ca59501342e159d1bb0b
nicola-calonaci <nicola.calonaci@gmail.com>
2022-10-28 11:29:25 +0200

    solved bug in purity estimate

--- R/purity_from_fit.R
+++ R/purity_from_fit.R
@@ -4,8 +4,8 @@ purity_from_fit = function(n_binomials, peaks, purity, eps) {
     out = peaks[2] * 2
   if (n_binomials == 1)
     out = peaks[1] * 2
-  if (n_binomials == 2 & !is.na(purity) & min(abs(peaks[2] * 2 - purity), abs(peaks[1] * 2 - purity)) <= eps)
-    out = peaks[which.min(c(abs(peaks[2] * 2 - purity), abs(peaks[1] * 2 - purity)))] * 2
+  if (n_binomials == 2 & !is.na(purity) & min(abs(peaks[1] * 2 - purity), abs(peaks[2] * 2 - purity)) <= eps) 
+    out = peaks[which.min(c(abs(peaks[1] * 2 - purity), abs(peaks[2] * 2 - purity)))] * 2
   if (is.na(out)) {
     cli::cli_alert("Purity could not be estimated reliably. Check results of the fit in the output figure")
   }
