35672332a1e7fcdd8439c05bfccb9bea961e5b03
nicola-calonaci <nicola.calonaci@gmail.com>
2022-11-03 17:21:26 +0100

    typo on last fix

--- R/purity_from_fit.R
+++ R/purity_from_fit.R
@@ -4,12 +4,17 @@ purity_from_fit = function(n_binomials, peaks, purity, eps) {
     out = peaks[2] * 2
   if (n_binomials == 1)
     out = peaks[1] * 2
-  if (n_binomials == 2){
-    purity_hyp = c(min(peaks[1] * 2, 1), min(peaks[2] * 2, 1)) # in this case we have two equivalent putative purities 
-    if(!is.na(purity) & min(abs(purity_hyp-purity)) <= eps) # check if at least one is close to the input one
-    out = purity_hyp[which.min(abs(purity_hyp-purity))] # choose the estimate that is closest to input 
-  if (is.na(out)) {
-    cli::cli_alert("Purity could not be estimated reliably. Check results of the fit in the output figure")
+  if (n_binomials == 2) {
+    purity_hyp = c(min(peaks[1] * 2, 1), min(peaks[2] * 2, 1)) # in this case we have two equivalent putative purities
+    if (!is.na(purity) &
+        min(abs(purity_hyp - purity)) <= eps)
+      # check if at least one is close to the input one
+      out = purity_hyp[which.min(abs(purity_hyp - purity))] # choose the estimate that is closest to input
+    if (is.na(out)) {
+      cli::cli_alert(
+        "Purity could not be estimated reliably. Check results of the fit in the output figure"
+      )
+    }
   }
   return(out)
 }
