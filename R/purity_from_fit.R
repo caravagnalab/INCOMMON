ca9ce4cedf707975a5f9fc675876e44d20cbe8d6
nicola-calonaci <nicola.calonaci@gmail.com>
2022-11-03 17:15:20 +0100

    Bug fix for purity estimates lower than observed VAFs

--- R/purity_from_fit.R
+++ R/purity_from_fit.R
@@ -4,8 +4,10 @@ purity_from_fit = function(n_binomials, peaks, purity, eps) {
     out = peaks[2] * 2
   if (n_binomials == 1)
     out = peaks[1] * 2
-  if (n_binomials == 2 & !is.na(purity) & min(abs(peaks[1] * 2 - purity), abs(peaks[2] * 2 - purity)) <= eps) 
-    out = peaks[which.min(c(abs(peaks[1] * 2 - purity), abs(peaks[2] * 2 - purity)))] * 2
+  if (n_binomials == 2){
+    purity_hyp = c(min(peaks[1] * 2, 1), min(peaks[2] * 2, 1)) # in this case we have two equivalent putative purities 
+    if(!is.na(purity) & min(abs(purity_hyp-purity)) <= eps) # check if at least one is close to the input one
+    out = purity_hyp[which.min(abs(purity_hyp-purity))] # choose the estimate that is closest to input 
   if (is.na(out)) {
     cli::cli_alert("Purity could not be estimated reliably. Check results of the fit in the output figure")
   }
