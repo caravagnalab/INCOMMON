be6a0c13477bc42caf833afbdf059fa25437dc4d
nicola-calonaci <nicola.calonaci@gmail.com>
2024-03-26 11:47:37 +0100

    change required input and initialisation

--- R/data.R
+++ R/data.R
@@ -1,61 +1,40 @@
-#' Example INCOMMON dataset.
-#' @docType data
+#' Genomic data of the MSK-MetTropism cohort
+#' @docType Data
 #'
-#' @usage data(example_data)
+#' @description This table contains sample names and corresponding read counts data from the MSK-MetTropism cohort.
 #'
-#' @description Genomic data from sample P-0002081-T01-IM3 of the MSK-MET cohort.
-#' 
-#' @format A named list with the following components:
-#' \describe{
-#'   \item{\code{mutations}}{A data frame containing mutations from sample P-0002081-T01-IM3 
-#'   of the MSK-MET cohort with columns `chr`, `from`, `to`, `ref`, `alt`, `gene`, `NV`, `DP` and `VAF`.}
-#'   \item{\code{sample}}{The name of the sample.}
-#'   \item{\code{tumor_type}}{The tumor type of the sample.}
-#'   \item{\code{purity}}{The purity of the tumor sample (fraction).}
-#'}
+#' @usage data(MSK_genomic_data)
 #'
-#' @details The columns in the data frame (\code{mutations}) are:
-#' \describe{
-#'   \item{chr}{Chromosome harbouring the mutation.}
-#'   \item{from}{Genomic start position of the mutation.}
-#'   \item{to}{Genomic end position of the mutation.}
-#'   \item{ref}{Reference allele.}
-#'   \item{alt}{Alternative allele.}
-#'   \item{gene}{Gene harbouring the mutation.}
-#'   \item{NV}{Number of reads with the variant}
-#'   \item{DP}{Total number of reads (sequencing depth)}
-#'   \item{VAF}{Variant allele frequency of the mutation.}
-#' }
-#' 
-#' @examples
-#' data(example_data)
-#' example_data
-#' \dontrun{
-#' # Create an example data
-#' example <- list(
-#' mutations <- data.frame(
-#'  chr = c("chr1", "chr14", "chr17"),  # Chromosome column
-#'  from = c(16265908, 105246551, 7578503),  # Start position column
-#'  to = c(16265908, 105246551, 7578518),  # End position column
-#'  ref = c("A", "C", "CAGGGCAGGTCTTGGC"),  # Reference allele column
-#'  alt = c("T", "T", "-"),  # Alternate allele column
-#'  gene = c("SPEN", "AKT1", "TP53")  # Gene column
-#' ),
-#'   sample = "P-0000004-T01-IM3",
-#'   purity = 0.5
-#' )
-#' }
+#' @format A tibble with 224939 rows and 10 columns:
 #'
 #' @source MSK-MET at cBioPortal https://www.cbioportal.org/study/summary?id=msk_met_2021
-"example_data"
+#' @examples
+#' data(MSK_genomic_data)
+#' MSK_genomic_data
+"MSK_genomic_data"
+
+#' Genomic data of the MSK-MetTropism cohort
+#' @docType Data
+#'
+#' @description This table contains sample names and corresponding clinical data from the MSK-MetTropism cohort.
+#'
+#' @usage data(MSK_clinical_data)
+#'
+#' @format A tibble with 25659 rows and 15 columns:
+#'
+#' @source MSK-MET at cBioPortal https://www.cbioportal.org/study/summary?id=msk_met_2021
+#' @examples
+#' data(MSK_clinical_data)
+#' MSK_clinical_data
+"MSK_clinical_data"
 
 #' Priors from PCAWG
 #' @docType data
 #'
 #' @usage data(pcawg_priors)
-#' 
+#'
 #' @description Prior distribution of copy number and mutation multiplicity from PCAWG.
-#' 
+#'
 #' @format A data frame with 4466 rows and 4 columns:
 #' \describe{
 #'   \item{gene}{Name of the gene (Hugo Symbol).}
@@ -73,11 +52,11 @@
 #' @docType Data
 #'
 #' @description This table contains gene roles in cancer, as obtained from the COSMIC Cancer
-#' Gene Census v98. Data were curated such that each gene in list is assigned 
+#' Gene Census v98. Data were curated such that each gene in list is assigned
 #' either TSG or oncogene.
-#' 
+#'
 #' @usage data(cancer_gene_census)
-#' 
+#'
 #' @format A data frame with 733 rows and 2 columns:
 #' \describe{
 #'   \item{gene}{Name of the gene (Hugo Symbol)}
@@ -88,19 +67,3 @@
 #' data(cancer_gene_census)
 #' cancer_gene_census
 "cancer_gene_census"
-
-#' Mutation data of the MSK-MetTropism cohort
-#' @docType Data
-#'
-#' @description This table contains read counts data and other information
-#' from the MSK-MetTropism cohort.
-#' 
-#' @usage data(data_MSK)
-#' 
-#' @format A data frame with 224890 rows and 21 columns:
-#' 
-#' @source MSK-MET at cBioPortal https://www.cbioportal.org/study/summary?id=msk_met_2021
-#' @examples
-#' data(data_MSK)
-#' data_MSK
-"data_MSK"
--- R/init.R
+++ R/init.R
@@ -1,29 +1,109 @@
 #' Prepare input for analyses with \code{'INCOMMON'}.
 #' @description
 #' Process input data into an object of class \code{'INCOMMON'}, ready for
-#' dwonstream analyses (e.g. `classify_sample`).
-#' @param mutations a tibble with columns mutant chromosome `chr`, mutation start position `from`, mutation end position `to`,
-#'   reference allel `ref`, alternative allele `alt`, integer sequencing depth `DP`, integer number
-#'   of reads with variant `NV`, variant allelic frequency `VAF` and gene name `gene`
-#'   as Hugo Symbol.
-#' @param sample Sample name or ID.
-#' @param tumor_type Tumor type of the sample.
-#' @param purity Purity of the sample.
-#' @param gene_roles A tibble reporting a list of `gene` names and associated
-#'  `gene_role` among "oncogene", "TSG" and "fusion". The default is taken from COSMIC Cancer Gene Census.
+#' downstream analyses (e.g. `classify`).
+#' @param genomic_data a data table of annotated mutations with columns sample name `sample`, mutant chromosome `chr`, mutation start position `from`, mutation end position `to`,
+#'   reference allele `ref`, alternative allele `alt`, integer sequencing depth `DP`, integer number
+#'   of reads with variant `NV`, variant allele frequency `VAF` and gene name `gene`
+#'   as Hugo Symbol,  protein sequence of the variant in HGVS recommended format, preferably 1-letter amino-acid code `HGVSp_Short`.
+#' @param clinical_data a data table of clinical data with compulsory matching sample names `sample` and sample purity `purity`, and optional
+#' clinical features like tumor type ONCOTREE code `tumor_type` (required for tumor specific priors), overall survival status `OS_STATUS` and time `OS_MONTHS`
+#' (required for survival analysis), `SAMPLE_TYPE` (Primary or Metastasis) and  number of metastases `MET_COUNT` (required for metastatic propensity analysis),
+#' metastatic site `METASTATIC_SITE` (required for metastatic tropism analysis), plus any other useful covariate.
+#' @param gene_roles A data table reporting `gene` names and associated `gene_role` ("oncogene" or "TSG"). The default is taken from COSMIC Cancer Gene Census v98.
 #' @return An object of class \code{INCOMMON}.
 #' @export
-#' @examples 
-#' input = init(mutations = example_data$data, sample = example_data$sample, purity = example_data$purity, tumor_type = example_data$tumor_type)
+#' @examples
+#' input = init(genomic_data = MSK_genomic_data, clinical_data = MSK_clinical_data)
 #' print(input)
-init = function(mutations, sample, tumor_type, purity, gene_roles = INCOMMON::cancer_gene_census){
-  mutations = dplyr::left_join(mutations, gene_roles, by = "gene")
-  out = list(
-    data = mutations,
-    sample = sample,
-    purity = purity,
-    tumor_type = tumor_type
-  )
+init = function(genomic_data,
+                clinical_data,
+                gene_roles = INCOMMON::cancer_gene_census) {
+  # Add gene roles
+  genomic_data = genomic_data %>%
+    dplyr::left_join(gene_roles, by = "gene")
+  # Create object as list
+  out = list(genomic_data = genomic_data,
+             clinical_data = clinical_data)
+  # Add class specification
   class(out) = "INCOMMON"
+
+  ## Print input data
+  cli::cli_rule(
+    paste(
+      crayon::bgMagenta(crayon::black("[ INCOMMON ] ")),
+      'Inference of copy number and mutation multiplicity in oncology'
+    )
+  )
+
+  # Genomic data summary
+
+  n_samples = out$genomic_data$sample %>% unique() %>% length()
+  n_mutations = out$genomic_data %>% nrow()
+  n_genes = out$genomic_data$gene %>% unique() %>% length()
+
+  na_gene_names = dplyr::filter(out$genomic_data, is.na(gene)) %>% nrow()
+  na_role_genes = dplyr::filter(out$genomic_data, is.na(gene_role)) %>% dplyr::pull(gene) %>% unique() %>% length()
+  no_reads_mutations = dplyr::filter(out$genomic_data, is.na(NV) | is.na(DP)) %>% nrow()
+  no_reads_samples = dplyr::filter(out$genomic_data, is.na(NV) | is.na(DP)) %>% dplyr::pull(sample) %>% unique() %>% length()
+
+  cli::cli_h2("Genomic data")
+
+  cli::cli_alert_success(
+    "Found {.field {n_samples}} samples, with {.field {n_mutations}} mutations in {.field {n_genes}} genes"
+  )
+
+  if(no_reads_mutations != 0)
+    cli::cli_alert_warning("No read counts found for {.field {no_reads_mutations}} mutations in {.field {no_reads_samples}} samples")
+
+  if(na_gene_names != 0)
+    cli::cli_alert_warning("Gene name not provided for {.field {na_gene_names}} mutations")
+
+  if(na_role_genes != 0)
+    cli::cli_alert_warning("{.field {na_role_genes}} genes could not be assigned a role (TSG or oncogene)")
+
+  mean_depth = genomic_data$DP %>% mean(na.rm = T) %>% as.integer()
+
+  cli::cli_alert_info('Average sequencing depth: {.field {mean_depth}}')
+
+  # Clinical data summary
+
+  clinical_features = colnames(out$clinical_data)
+
+  cli::cli_h2("Clinical data")
+
+  cli::cli_alert_info(
+    "Provided clinical features:"
+  )
+  cat('\n')
+
+  missing_features = c()
+
+  for(x in c('sample', 'purity')){
+    if(x %in% clinical_features) {
+      cli::cli_bullets(c('v' = paste(x, '(required for classification)')))
+    } else{
+      cli::cli_bullets(c('x' = paste(x, '(required for classification)')))
+      missing_features = append(x, missing_features)
+    }
+  }
+
+  for(x in setdiff(clinical_features, c('sample', 'purity'))){cli::cli_bullets(c('v' = paste(x)))}
+
+  cat('\n')
+
+
+  if(!is.null(missing_features)) cli::cli_alert_warning('Missing features {.field {missing_features}} required for classifcation')
+
+  matching_samples = intersect(genomic_data$sample, clinical_data$sample) %>% unique() %>% length()
+  unmatched_samples = c(setdiff(genomic_data$sample, clinical_data$sample), setdiff(clinical_data$sample, genomic_data$sample)) %>% unique() %>% length()
+
+  cli::cli_alert_success("Found {.field {matching_samples}} matching samples")
+  if(unmatched_samples != 0) cli::cli_alert_danger("Found {.field {unmatched_samples}} matching samples") else  cli::cli_alert_success("No mismatched samples")
+
+  mean_purity = clinical_data$purity %>% mean(na.rm = T) %>% round(2)
+
+  cli::cli_alert_info('Average sample purity: {.field {mean_purity}}')
+
   return(out)
 }
