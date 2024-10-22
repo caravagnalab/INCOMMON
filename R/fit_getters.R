8096a6db60e7389021516eb212b6f912a2b9762a
nicola-calonaci <nicola.calonaci@gmail.com>
2024-10-22 12:56:35 +0200

    utils to extract stan samples

--- R/fit_getters.R
+++ R/fit_getters.R
@@ -35,7 +35,8 @@ get_map_x = function(x, sample){
 
 
 classification = function(x){
-  samples = input(x) %>% dplyr::pull(sample) %>% unique()
+  samples = names(x$classification$fit)
+  x = subset_sample(x = x, sample_list = samples)
   lapply(samples, function(s){
     what = get_map_z_km(x = x, sample = s)
     # what$sample = s
@@ -46,3 +47,44 @@ classification = function(x){
     dplyr::bind_cols(x$input) %>%
     dplyr::select(sample, purity, chr, from, to , ref, alt, gene, gene_role, NV, DP, VAF, k, m, z_km, purity_fit, x_fit, dplyr::everything())
 }
+
+get_N_rep = function(x, sample){
+  x$classification$fit[[sample]]$N_rep
+}
+
+get_n_rep = function(x, sample){
+  x$classification$fit[[sample]]$n_rep
+}
+
+
+get_N_rep_ci = function(x, sample) {
+  N_rep = get_N_rep(x = x, sample = sample) %>% as.vector()
+  dplyr::tibble(
+    N = N_rep,
+    k = get_k_m_rep(x = x, sample = sample)$k
+  ) %>%
+    dplyr::group_by(k) %>%
+    dplyr::reframe(mean = mean(N), q5 = quantile(N, probs = .05), q95 = quantile(N, probs = .95))
+}
+
+get_k_m_rep = function(x, sample){
+
+  k_max = x$classification$parameters$k_max
+
+  z_km = x$classification$fit[[sample]]$z_km
+
+  k_m_table = expand.grid(k = 1:k_max, m = 1:k_max) %>%
+    dplyr::as_tibble() %>%
+    dplyr::filter(m <= k) %>% arrange(k, m)
+
+  km_idx = apply(z_km, c(1, 2), which.max)
+
+  dplyr::tibble(
+    k = k_m_table$k[as.vector(km_idx)],
+    m = k_m_table$m[as.vector(km_idx)]
+  )
+
+}
+
+
+
